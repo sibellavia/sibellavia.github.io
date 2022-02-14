037a2ccbbfd010869c8b4c811af754feb71916f4
Simone Bellavia <simonebellavia@Simones-MBP.fritz.box>
2022-02-14 16:53:16 +0100

    deleted nextjs folder

--- old-website-with-nextjs/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/global.css'
-import './coding-projects/password-generator-style.css'
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
