76115c99e0eff820f33b4b8a8328e8f61da10814
simoneb1x <moneb1x@gmail.com>
2023-05-19 20:42:32 +0200

    another bio change

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,24 +21,32 @@ export default function About() {
         </p>
 
         <p className="text-gray-700 dark:text-gray-300">
-          I am currently working as a Scrum Master and Software Engineer on several
-          projects. Web Development and DevOps engineering have characterized my
-          work experience so far: I am used to working in complex contexts based
-          on microservice architectures, where I am committed to coordinating my
-          teams and participating in the design and development of the solutions
-          we propose. The industries I have worked in are Energy, Public
-          Administration, Chemical-Cosmetic, and E-Commerce.
+          I am currently working as a Scrum Master and Software Engineer on
+          several projects. Web Development and DevOps engineering have
+          characterized my work experience so far: I am used to working in
+          complex contexts based on microservice architectures, where I am
+          committed to coordinating my teams and participating in the design and
+          development of the solutions we propose. The industries I have worked
+          in are Energy, Public Administration, Chemical-Cosmetic, and
+          E-Commerce.
         </p>
 
         <p className="text-gray-700 dark:text-gray-300">
           I study Data Science because it is the perfect synthesis of my main
           passions: programming, mathematics, statistics, business development.
           My biggest dream is to one day work in healthcare and oncology in
-          general. In the past I also had a prominent role as a UX/UI
-          designer: it taught me to love symmetries, well-chosen typographies,
-          and good taste in design. I am in love with the sea, the waves, the
-          wind and... the gym! I train every day to land in the world of
-          powerlifting. And whenever I can, I go into the water to surf.
+          general. In the past I also had a prominent role as a UX/UI designer:
+          it taught me to love symmetries, well-chosen typographies, and good
+          taste in design.
+        </p>
+
+        <p className="text-gray-700 dark:text-gray-300">
+          I am in love with the sea, the waves, the wind and... the gym! I train
+          every day to land in the world of powerlifting. And whenever I can, I
+          go into the water to surf and SUP. I am very curious. I love to read
+          and study. I am in love with math and physics. My favorite colors are
+          coral and black. My favorite flower is the sunflower. My favorite
+          fruit is the pear. I prefer morning to evening. I love to travel.
         </p>
 
         <h2>Skills 👨🏼‍💻</h2>
@@ -63,7 +71,10 @@ export default function About() {
         <ul className="list-disc text-gray-700 dark:text-gray-300 pl-8">
           <li>Agile: PAL I, PSM I, PSPO I, SPS, SAFe 5 Practitioner</li>
           <li>CRM: Salesforce Administrator</li>
-          <li>Languages: English C1, French B2, <i>Spanish A2 self-evaluated</i>, Italian mother tongue</li>
+          <li>
+            Languages: English C1, French B2, <i>Spanish A2 self-evaluated</i>,
+            Italian mother tongue
+          </li>
         </ul>
 
         <h2>Let&apos;s get in touch 😊</h2>
