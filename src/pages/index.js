98a0ecd5c464e31b7b4995c9744668ebceeccb01
simoneb1x <moneb1x@gmail.com>
2023-11-24 18:51:03 +0100

    escapes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ export default function Home({ allPostsData }) {
   return (
     <Layout>
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold my-4">Simone Bellavia's Web Page</h1>
+        <h1 className="text-2xl font-bold my-4">Simone Bellavia&apos;s Web Page</h1>
         <div className="flex flex-wrap -mx-2">
           <div className="w-full md:w-1/2 px-2 md:mt-0 mt-4 mb-8">
             <h2 className="text-xl font-bold mb-2">about</h2>
             <p className="text-gray-700 dark:text-gray-300">
-          hey I'm a 25 y/o working at Accenture as a Senior Analyst and
-          Software Engineer in awesome international teams. I'm also a Data Science
-          student at the University of Milan-Bicocca. when I'm not writing code, you
-          can find me at the nearest beach or in a gym. I'm interested in data science,
+          hey I&apos;m a 25 y/o working at Accenture as a Senior Analyst and
+          Software Engineer in awesome international teams. I&apos;m also a Data Science
+          student at the University of Milan-Bicocca. when I&apos;m not writing code, you
+          can find me at the nearest beach or in a gym. I&apos;m interested in data science,
           software engineering, data science, project management, healthcare.
           <br></br>
           <Link href="/about">
