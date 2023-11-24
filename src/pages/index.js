37ed5c166b0ec4203f431167123470f585a00f9e
simoneb1x <moneb1x@gmail.com>
2023-11-24 18:48:58 +0100

    new style

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,104 +5,39 @@ import { getSortedPostsData } from "../../lib/posts";
 export default function Home({ allPostsData }) {
   return (
     <Layout>
-      {/* <header className="flex justify-end py-4">
-        <DarkModeButton />
-      </header>
-       */}
-      <div className="space-y-4">
-        <h1 className="text-2xl">Simone Bellavia</h1>
-        <p className="text-gray-700 dark:text-gray-300">
-          Hey 🌻
-          <br></br>I am a 25 y/o working at Accenture as a Senior Analyst and
-          Software Engineer in awesome international teams. I am a Data Science
-          student at the University of Milan-Bicocca.
-          <br></br>I like sports and I am in love with the sea. You can often
-          find me at the beach, even in winter. Interested in data science,
-          software engineering, technical agility, healthcare, project
-          management, Unix.
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-bold my-4">Simone Bellavia's Web Page</h1>
+        <div className="flex flex-wrap -mx-2">
+          <div className="w-full md:w-1/2 px-2 md:mt-0 mt-4 mb-8">
+            <h2 className="text-xl font-bold mb-2">about</h2>
+            <p className="text-gray-700 dark:text-gray-300">
+          hey I'm a 25 y/o working at Accenture as a Senior Analyst and
+          Software Engineer in awesome international teams. I'm also a Data Science
+          student at the University of Milan-Bicocca. when I'm not writing code, you
+          can find me at the nearest beach or in a gym. I'm interested in data science,
+          software engineering, data science, project management, healthcare.
           <br></br>
           <Link href="/about">
             <span className="text-blue-600 dark:text-blue-400 hover:underline">
-              Read more about me here :)
+              read more about me here :)
             </span>
           </Link>
         </p>
-      </div>
-
-      {/* <div className="space-y-4">
-        <h1 className="text-xl">Side-projects</h1>
-        <div className="grid grid-cols-1 gap-6">
-          <div>
-            <a
-              href="https://infotumori.it/"
-              target="_blank"
-              className="text-lg font-semibold text-blue-600 dark:text-blue-400 hover:underline"
-            >
-              Infotumori.it
-            </a>
-            <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-              Infotumori aims to provide essential information and key resources
-              for proper cancer awareness. It is aimed at people who want to get
-              informed, prevent and cure. Developed for an Italian target
-              audience. Its development is still in progress.
-            </p>
           </div>
-          <div>
-            <a
-              href="https://github.com/simoneb1x/zen"
-              target="_blank"
-              className="text-lg font-semibold text-blue-600 dark:text-blue-400 hover:underline"
-            >
-              Zen
-            </a>
-            <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-              A minimalist, simple and essential timer. Yet another Pomodoro app
-              for macOS, developed using Swift.
-            </p>
-          </div>
-          <div>
-            <Link href="/bemydoc">
-              <span className="text-lg font-semibold text-blue-600 dark:text-blue-400 hover:underline">
-                beMyDoc
-              </span>
-            </Link>
-            <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-              beMyDoc is the design of an application for monitoring the health
-              status of leukemia patients to whom chemotherapy protocols can be
-              applied by administering oral drugs.
-            </p>
+          <div className="w-full md:w-1/2 px-2">
+            <h2 className="text-xl font-bold mb-2">posts</h2>
+            <ul className="space-y-4">
+              {allPostsData.map(({ slug, title, date }) => (
+                <li key={slug} className="lowercase">
+                  <ul><Link href={`/posts/${slug}`}>
+                      {title}
+                  </Link></ul>
+                  
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </div> */}
-
-      <div className="space-y-4">
-        <h1 className="text-xl">Last posts</h1>
-        <ul className="mt-8 space-y-4">
-          {allPostsData.map(({ slug, title, date }) => (
-            <li key={slug}>
-              <a
-                href={`/posts/${slug}`}
-                className="text-lg font-semibold text-blue-600 dark:text-blue-400 hover:underline"
-              >
-                {title}
-              </a>
-              <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {date}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="space-y-4">
-        <h1 className="text-sm text-gray-500">
-          © 2019-23 made with ❤️ by{" "}
-          <Link href="https://twitter.com/simoneblv">
-            <span className="text-blue-600 dark:text-blue-400 hover:underline">
-              @simoneblv
-            </span>
-          </Link>
-        </h1>
       </div>
     </Layout>
   );
